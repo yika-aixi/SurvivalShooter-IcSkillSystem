6114cf9fff3e9f6f1011d8e437f90e122993a080
Icarus <1375400884@qq.com>
2019-12-08 02:45:14 +0800

    !A 技能行为组件

---
+++ Assets/_Complete-Game/Scripts/Skill/CSkillsBehaveComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using CabinIcarus.IcSkillSystem.Expansion.Builtin.Skills.Component;
+using CabinIcarus.Joystick.Evetns;
+using UnityEngine;
+
+namespace Scripts.Skill
+{
+    public class CSkillsBehaveComponent : SkillBehaveComponent
+    {
+        public NoParEvent OnUse = new NoParEvent();
+
+        public FloatParEvent OnSetCooling = new FloatParEvent();
+        
+        public float CoolingTime;
+
+        [SerializeField]
+        private string _behaveComKey = "behave";
+
+        protected override void Init()
+        {
+            base.Init();
+            
+            OnSetCooling?.Invoke(CoolingTime);
+        }
+
+        void UseSkill()
+        {
+            OnUse?.Invoke();
+        }
+
+        public override void Use(Dictionary<string, object> data)
+        {
+            CurrentSkill.RootNode.Blackboard.Set(_behaveComKey,this);
+            base.Use(data);
+        }
+    }
+}
